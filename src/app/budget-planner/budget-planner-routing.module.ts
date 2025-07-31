c482e6f172cd69a7d060a3885f0ad57e717039c5
SayedThoha <147703696+SayedThoha@users.noreply.github.com>
2025-07-31 23:47:09 +0530

    routing fixed

--- src/app/budget-planner/budget-planner-routing.module.ts
+++ src/app/budget-planner/budget-planner-routing.module.ts
@@ -10,6 +10,7 @@ import { HistoryComponent } from './history/history.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'side-nav', component: SideNavComponent },
   { path: 'dashboard', component: DashboardComponent },
@@ -18,6 +19,7 @@ const routes: Routes = [
   { path: 'todo', component: TodoComponent },
   { path: 'history', component: HistoryComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
